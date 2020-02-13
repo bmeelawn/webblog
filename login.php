c52566d8da95e51574ef74e61a6f74da95103559
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-13 14:28:37 +0545

    add code for front end

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+include 'includes/header.inc.php';
+include 'includes/navbar.inc.php';
+?>
+<main class="box login-form max-width gutterY">
+    <section class="row">
+        <div class="col-sm-12 col-md-12">
+            <h1 class="title big w-600">Login</h1>
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="uid" placeholder="Email/Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <a href="" class="text-color text-dim">Forgot your password</a>
+                <button type="submit" name="login-submit" class="btn">Login</button>
+            </form>
+        </div>
+    </section>
+
+</main>
+<?php
+include 'includes/footer.inc.php';
+?>
---
+++ signup.php
@@ -0,0 +1,46 @@
+<?php
+include 'includes/header.inc.php';
+include 'includes/navbar.inc.php';
+?>
+<main class="box login-form max-width gutterY">
+    <section class="row">
+        <div class="col-sm-12 col-md-12">
+            <h1 class="title big w-600">Signup</h1>
+            <form action="includes/signup.inc.php" method="post">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <input type="text" name="username" placeholder="Username">
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <input type="text" name="email" placeholder="Email">
+                    </div>
+
+                    <div class="col-md-6 col-sm-12">
+                        <input type="text" name="firstname" placeholder="Firstname">
+
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <input type="text" name="lastname" placeholder="Lastname">
+
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <input type="password" name="pwd" placeholder="Password">
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <input type="password" name="repeat-pwd" placeholder="Repeat Password">
+                    </div>
+                    <div class="ml-auto mr-auto">
+                        <button type="submit" name="signup-submit" class="btn">Signup</button>
+
+                    </div>
+
+                </div>
+
+            </form>
+        </div>
+    </section>
+
+</main>
+<?php
+include 'includes/footer.inc.php';
+?>

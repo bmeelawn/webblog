721ae9c23fa7161e9eddaea0ff9b8a5f4c020cd9
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-13 14:31:59 +0545

    add navigation

---
+++ includes/navbar.inc.php
@@ -0,0 +1,32 @@
+<header class="navbar-expand-md navbar-light bg-light" style="background: #fff !important">
+    <div class="max-width">
+        <nav class="navbar navbar-default">
+            <a class="navbar-brand" href="#">IOS DEV</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">Signup</a>
+                    </li>
+                    <form action="" method="post" class="search-box form-inline my-2 my-lg-0">
+                        <div class="input-holder">
+                            <input type="text" name="search" placeholder="Search">
+                            <button type="submit" name="search-submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                        </div>
+                    </form>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>

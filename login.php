f0caa043df685575c9cffa7d4cd6196d9c23fa22
bmeelawn <60973539+bmeelawn@users.noreply.github.com>
2020-02-13 16:31:26 +0545

    login

--- login.php
+++ login.php
@@ -1,13 +1,15 @@
 <?php
 include 'includes/header.inc.php';
 include 'includes/navbar.inc.php';
-include 'includes/db.inc.php';
+include 'includes/login.inc.php';
+
 ?>
 <main class="box login-form max-width gutterY">
     <section class="row">
         <div class="col-sm-12 col-md-12">
             <h1 class="title big w-600">Login</h1>
             <form action="includes/login.inc.php" method="post">
+                <?php echo $message  ?>
                 <input type="text" name="uid" placeholder="Email/Username">
                 <input type="password" name="pwd" placeholder="Password">
                 <a href="" class="text-color text-dim">Forgot your password</a>

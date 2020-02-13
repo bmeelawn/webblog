7acb284dfb6d98bf7e8fe0e21db19ac43f569351
bmeelawn <60973539+bmeelawn@users.noreply.github.com>
2020-02-13 16:32:00 +0545

    login validation

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,31 +1,42 @@
 <?php
 
-if (isset($_POST['submit'])) {
+include 'db.inc.php';
+
+$message = "";
+
+if (isset($_POST['login-submit'])) {
 
     $login_username = $_POST['uid'];
     $login_password = $_POST['pwd'];
 
-    $query = "SELECT * FROM users";
-    $all_users_query = mysqli_query($connection, $query);
 
-    while($row=mysqli_fetch($all_users_query)) {
+    $login_username = mysqli_real_escape_string($conn, $login_username);
+    $login_password = mysqli_real_escape_string($conn, $login_password);
 
-        $username = $row['username'];
-        $password = $row['user_password'];
+    $query = "SELECT * FROM users where username = '$login_username' OR user_email = '$login_username'";
+    $all_users_query = mysqli_query($conn, $query);
 
-        if ($username === $login_username || $email === $login_username) {
+    while($row=mysqli_fetch_assoc($all_users_query)) {
 
-            
+        if (count($row)<0) {
+
+            $message ="Password or Email Incorrect.";
 
         }
 
+        $password = $row['user_password'];
+
+        if ($password !== $login_password) {
+
+            $message = "Password incorrect";
+
+        } else {
+
+            $message = "You are successully login.";
+
+        }
+        
     }
-
-
-
-
-
-
 
 
 }

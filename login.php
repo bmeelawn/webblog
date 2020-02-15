a0274fee367e62424dac4d621ef38d659584ed4c
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-15 15:25:05 +0545

    add title

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+$title = "Login";
 include 'includes/header.inc.php';
 include 'includes/navbar.inc.php';
 include 'includes/login.inc.php';
@@ -14,10 +15,10 @@ if (isset($_SESSION['userid'])) {
             <h1 class="title big w-600">Login</h1>
             <form action="" method="post">
 
-            <?php if (isset($_POST['login-submit'])) {
+                <?php if (isset($_POST['login-submit'])) {
                     echo $message;
                 }
-            ?>
+                ?>
 
                 <input type="text" name="uid" placeholder="Email/Username">
                 <input type="password" name="pwd" placeholder="Password">

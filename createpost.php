04592c69eb3ab5460733d32a2bc179ba4c4b6323
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-15 15:27:25 +0545

    add create post page for creating post

---
+++ createpost.php
@@ -0,0 +1,24 @@
+<?php
+$title = "Create new post";
+include 'includes/header.inc.php';
+include 'includes/navbar.inc.php';
+?>
+<main class="max-width gutterY">
+    <section class="row">
+        <div class="col-sm-12 col-md-12">
+            <h1 class="title big w-600">Create new post</h1>
+            <form action="" method="post">
+                <input type="text" name="title" placeholder="Title">
+                <input type="text" name="categories" placeholder="Categories">
+                <textarea name="body" id="editor" cols="30" rows="10"></textarea>
+                <div class="ml-auto mr-auto">
+                    <button type="submit" name="createpost-submit" class="btn">Post</button>
+                </div>
+            </form>
+        </div>
+    </section>
+
+</main>
+<?php
+include 'includes/footer.inc.php';
+?>

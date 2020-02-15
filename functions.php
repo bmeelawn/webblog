b99dc63e200542a3edac334fee01f33e9f1d6fdb
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-15 17:31:10 +0545

    add getAllposts and getUsersPost function

---
+++ functions.php
@@ -0,0 +1,118 @@
+<?php
+
+function getAllPosts()
+{
+    global $conn;
+    $sql = "SELECT * FROM posts";
+    $res = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        while ($row = mysqli_fetch_assoc($res)) {
+
+            $post_category = $row['category'];
+            $post_title = $row['title'];
+            $post_body = $row['body'];
+            $new_trim_post = substr($post_body, 0, 137);
+            $post_author = $row['author'];
+            $date_year = date('yy', strtotime($row['created_at']));
+            $date_day = date('d', strtotime($row['created_at']));
+            $date_month = date('M', strtotime($row['created_at']));
+            echo "
+            <li class='col-md-12 col-sm-12'>
+                <a href=''>
+                    <div class='blog-card'>
+                        <div class='blog-body'>
+                            <h1 class='title small w-400 text-color text-dim text-upper'>
+                                $post_category
+                            </h1>
+                            <h1 class='title big w-600'>
+                                $post_title
+                            </h1>
+                            <p class='para p-x-small w-400 text-color text-dim'>
+                                $new_trim_post
+                            </p>
+                            <div class='user-details'>
+                                <a href='' class='para p-x-small w-400 text-color text-default text-capitalize'>$post_author</a>
+                                <p class='para p-x-small w-400 text-color text-dim'>$date_month $date_day, $date_year</p>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </a>
+            </li>
+              ";
+        }
+    } else {
+        echo "
+        <li class='col-md-12 col-sm-12'>
+            <a href=''>
+                <div class='blog-card'>
+                    <div class='blog-body'>
+                        <h1 class='title big w-600 text-center text-upper'>
+                            no posts to show
+                        </h1>
+                       
+                    </div>
+                </div>
+            </a>
+        </li>
+        ";
+    }
+}
+
+
+function getUserPosts()
+{
+    global $conn;
+    $userid = $_SESSION['userid'];
+    $sql = "SELECT * FROM posts WHERE user_id = $userid";
+    $res = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        while ($row = mysqli_fetch_assoc($res)) {
+            $post_category = $row['category'];
+            $post_title = $row['title'];
+            $post_body = $row['body'];
+            $new_trim_post = substr($post_body, 0, 137);
+            $post_author = $row['author'];
+            $date_year = date('yy', strtotime($row['created_at']));
+            $date_day = date('d', strtotime($row['created_at']));
+            $date_month = date('M', strtotime($row['created_at']));
+            echo "
+            <li class='col-md-12 col-sm-12'>
+                <a href=''>
+                    <div class='blog-card'>
+                        <div class='blog-body'>
+                            <h1 class='title small w-400 text-color text-dim text-upper'>
+                                $post_category
+                            </h1>
+                            <h1 class='title big w-600'>
+                                $post_title
+                            </h1>
+                            <p class='para p-x-small w-400 text-color text-dim'>
+                                $new_trim_post
+                            </p>
+                            <div class='user-details'>
+                                <a href='' class='para p-x-small w-400 text-color text-default text-capitalize'>$post_author</a>
+                                <p class='para p-x-small w-400 text-color text-dim'>$date_month $date_day, $date_year</p>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </a>
+            </li>
+              ";
+        }
+    } else {
+        echo "<li class='col-md-12 col-sm-12'>
+            <a href=''>
+                <div class='blog-card'>
+                    <div class='blog-body'>
+                        <h1 class='title big w-600 text-center text-upper'>
+                            no posts to show
+                        </h1>
+                       
+                    </div>
+                </div>
+            </a>
+        </li>";
+    }
+}

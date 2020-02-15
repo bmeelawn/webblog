10139cf1f98cb5adfdcc9c31c63534e946399147
bmeelawn <60973539+bmeelawn@users.noreply.github.com>
2020-02-15 17:35:01 +0545

    add createpost.inc.php in includes folder

---
+++ includes/createpost.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST['createpost-submit'])) {
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $categories = mysqli_real_escape_string($conn, $_POST['categories']);
+    $body = mysqli_real_escape_string($conn, $_POST['body']);
+    $user_id = $_SESSION['userid'];
+    $firstname = $_SESSION['firstname'];
+    $lastname = $_SESSION['lastname'];
+    $author = $firstname.$lastname;
+
+    if (empty($title) || empty($categories) || empty($body)) {
+        $message = "<p class='error text-center'>All field in required!</p>";
+    } else {
+        $query = "INSERT INTO posts(user_id, category, title, author, body, created_at) ";
+        $query.= "VALUES($user_id, '$categories', '$title', '$author',  '$body', now())";
+        $select_all_posts_query= mysqli_query($conn, $query);
+
+        if (!$select_all_posts_query) {
+            die("QUERY FAILED. ".mysqli_error($conn));
+        }
+        header("location: index.php");
+    }
+}
+
+?>

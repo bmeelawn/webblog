df2145b1c666ecee7c30cf678aaf8efc8a558750
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-15 15:20:53 +0545

    add script for text editor

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,3 +1,9 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.20.0/trumbowyg.min.js" integrity="sha256-oFd4Jr73mXNrGLxprpchHuhdcfcO+jCXc2kCzMTyh6A=" crossorigin="anonymous"></script>
+<script>
+    $('#editor').trumbowyg();
+</script>
+
 </body>
 
 </html>

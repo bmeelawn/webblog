061c85b08683db2dcb48bd6b32688f61d13ea6b9
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-15 17:54:05 +0545

    fix where author insert without space in between lastname and firstname

--- includes/createpost.inc.php
+++ includes/createpost.inc.php
@@ -7,20 +7,18 @@ if (isset($_POST['createpost-submit'])) {
     $user_id = $_SESSION['userid'];
     $firstname = $_SESSION['firstname'];
     $lastname = $_SESSION['lastname'];
-    $author = $firstname.$lastname;
+    $author = $firstname . ' ' . $lastname;
 
     if (empty($title) || empty($categories) || empty($body)) {
         $message = "<p class='error text-center'>All field in required!</p>";
     } else {
         $query = "INSERT INTO posts(user_id, category, title, author, body, created_at) ";
-        $query.= "VALUES($user_id, '$categories', '$title', '$author',  '$body', now())";
-        $select_all_posts_query= mysqli_query($conn, $query);
+        $query .= "VALUES($user_id, '$categories', '$title', '$author',  '$body', now())";
+        $select_all_posts_query = mysqli_query($conn, $query);
 
         if (!$select_all_posts_query) {
-            die("QUERY FAILED. ".mysqli_error($conn));
+            die("QUERY FAILED. " . mysqli_error($conn));
         }
         header("location: index.php");
     }
 }
-
-?>

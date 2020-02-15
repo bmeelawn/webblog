db03ac71ffea2e12eb70abf0109cd142a5355320
bmeelawn <60973539+bmeelawn@users.noreply.github.com>
2020-02-15 17:34:09 +0545

    include createpost.inc.php file in createpost.php

--- createpost.php
+++ createpost.php
@@ -2,12 +2,18 @@
 $title = "Create new post";
 include 'includes/header.inc.php';
 include 'includes/navbar.inc.php';
+include 'includes/createpost.inc.php';
 ?>
 <main class="max-width gutterY">
     <section class="row">
         <div class="col-sm-12 col-md-12">
             <h1 class="title big w-600">Create new post</h1>
             <form action="" method="post">
+            <?php 
+            if (isset($_POST['createpost-submit'])) {
+                echo $message;
+            }
+            ?>
                 <input type="text" name="title" placeholder="Title">
                 <input type="text" name="categories" placeholder="Categories">
                 <textarea name="body" id="editor" cols="30" rows="10"></textarea>

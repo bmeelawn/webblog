5102a8a056ad318a1c8eeebe636c0c264b7fad11
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-16 16:13:43 +0545

    function got get post details

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function getAllPosts()
     $res = mysqli_query($conn, $sql);
     if (mysqli_num_rows($res) > 0) {
         while ($row = mysqli_fetch_assoc($res)) {
-
+            $post_id = $row['post_id'];
             $post_category = $row['category'];
             $post_title = $row['title'];
             $post_body = $row['body'];
@@ -18,7 +18,7 @@ function getAllPosts()
             $date_month = date('M', strtotime($row['created_at']));
             echo "
             <li class='col-md-12 col-sm-12'>
-                <a href=''>
+                <a href='post.php?id=$post_id'>
                     <div class='blog-card'>
                         <div class='blog-body'>
                             <h5 class='title x-small w-400 text-color text-dim text-upper'>
@@ -115,5 +115,46 @@ function getUserPosts()
                 </div>
             </a>
         </li>";
+    }
+}
+
+function getPostById($post_id)
+{
+    global $conn;
+    $sql = "SELECT * FROM posts WHERE post_id = $post_id";
+    $res = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($res)) {
+        $post_category = $row['category'];
+        $post_title = $row['title'];
+        $post_body = $row['body'];
+        $post_author = $row['author'];
+        $date_year = date('yy', strtotime($row['created_at']));
+        $date_day = date('d', strtotime($row['created_at']));
+        $date_month = date('M', strtotime($row['created_at']));
+        echo "
+            <li class='col-md-12 col-sm-12'>
+                <a href=''>
+                    <div class='blog-card'>
+                        <div class='blog-body'>
+                            <div class='blog-body'>
+                            <h5 class='title x-small w-400 text-color text-dim text-upper'>
+                                $post_category
+                            </h5>
+                            <h1 class='title big w-600'>
+                                $post_title
+                            </h1>
+                            <p class='post-paragraph'>
+                            $post_body
+                            </p>
+                            <div class='user-details'>
+                                <a href='' class='para p-x-small w-400 text-color text-default text-capitalize'>$post_author</a>
+                                <h5 class='para p-x-small w-400 text-color text-dim'>$date_month $date_day, $date_year</h5>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </a>
+            </li>
+              ";
     }
 }

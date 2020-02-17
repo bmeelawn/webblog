f369b0885751796cdb2bb8615c821d8c4a052bcf
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-17 21:31:38 +0545

    update post working without bug

--- edit.php
+++ edit.php
@@ -1,16 +1,24 @@
 <?php
+
 $title = 'edit post';
 include 'includes/header.inc.php';
 include 'includes/navbar.inc.php';
 include 'functions.php';
+include 'includes/editpost.inc.php';
 ?>
 
 <main class="max-width gutterY">
     <section class="row">
-        <?php
-        $post_id = $_GET['id'];
-        editPost($post_id);
-        ?>
+        <div class='col-sm-12 col-md-12'>
+            <h1 class='title big w-600'>Edit post</h1>
+            <?php
+            if (isset($_POST['updatepost-submit'])) {
+                echo $message;
+            }
+            $post_id = $_GET['id'];
+            editPost($post_id);
+            ?>
+        </div>
     </section>
 </main>
 <?php

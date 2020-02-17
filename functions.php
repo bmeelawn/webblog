a67c17384bcc6b84fd72af34fd244b8f1764add7
bmeelawn <60973539+bmeelawn@users.noreply.github.com>
2020-02-17 22:15:32 +0545

    add getSearchPostsUsr and getSearchPosts function

--- functions.php
+++ functions.php
@@ -59,8 +59,143 @@ function getAllPosts()
     }
 }
 
+function getSearchPosts() 
+{
+    global $conn;
+    if (isset($_POST['search-submit'])) {
+        $search_value = $_POST['search'];
+        $query = "SELECT * FROM posts WHERE author like '%$search_value%' OR title like '%$search_value%'";
+        $search_query = mysqli_query($conn, $query);
+    
+        if (!$search_query) {
+            die("QUERY FAILED. " . mysqli_error($conn));
+        } else {
+    if (mysqli_num_rows($search_query) > 0) {
+        while ($row = mysqli_fetch_assoc($search_query)) {
+            $post_id = $row['post_id'];
+            $post_category = $row['category'];
+            $post_title = $row['title'];
+            $post_body = $row['body'];
+            $new_trim_post = substr($post_body, 0, 137);
+            $post_author = $row['author'];
+            $date_year = date('yy', strtotime($row['created_at']));
+            $date_day = date('d', strtotime($row['created_at']));
+            $date_month = date('M', strtotime($row['created_at']));
 
-function getUserPosts()
+            echo "
+            <li class='col-md-12 col-sm-12'>
+                <a href='post.php?id=$post_id'>
+                    <div class='blog-card'>
+                        <div class='blog-body'>
+                            <h5 class='title x-small w-400 text-color text-dim text-upper'>
+                                $post_category
+                            </h5>
+                            <h1 class='title big w-600'>
+                                $post_title
+                            </h1>
+                            <h5 class='paragraph p-x-small w-400 text-color text-dim'>
+                                $new_trim_post
+                            </h5>
+                            <div class='user-details'>
+                                <a href='' class='para p-x-small w-400 text-color text-default text-capitalize'>$post_author</a>
+                                <h5 class='para p-x-small w-400 text-color text-dim'>$date_month $date_day, $date_year</h5>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </a>
+            </li>
+              ";
+        }
+    } else {
+        echo "
+        <li class='col-md-12 col-sm-12'>
+            <a href=''>
+                <div class='blog-card'>
+                    <div class='blog-body'>
+                        <h1 class='title big w-600 text-center text-upper'>
+                            no posts to show
+                        </h1>
+                       
+                    </div>
+                </div>
+            </a>
+        </li>
+        ";
+    }
+        }
+    }
+}
+
+function getSearchPostsUsr() 
+{
+    global $conn;
+    if (isset($_POST['search-submit'])) {
+        $search_value = $_POST['search'];
+        $query = "SELECT * FROM posts WHERE user_id=".$_SESSION['userid']." AND title like '%$search_value%' OR body like '%$search_value%'";
+        $search_query = mysqli_query($conn, $query);
+    
+    if (!$search_query) {
+        die("QUERY FAILED. " . mysqli_error($conn));
+    } else {
+    if (mysqli_num_rows($search_query) > 0) {
+        while ($row = mysqli_fetch_assoc($search_query)) {
+            $post_id = $row['post_id'];
+            $post_category = $row['category'];
+            $post_title = $row['title'];
+            $post_body = $row['body'];
+            $new_trim_post = substr($post_body, 0, 137);
+            $post_author = $row['author'];
+            $date_year = date('yy', strtotime($row['created_at']));
+            $date_day = date('d', strtotime($row['created_at']));
+            $date_month = date('M', strtotime($row['created_at']));
+
+            echo "
+            <li class='col-md-12 col-sm-12'>
+                <a href='post.php?id=$post_id'>
+                    <div class='blog-card'>
+                        <div class='blog-body'>
+                            <h5 class='title x-small w-400 text-color text-dim text-upper'>
+                                $post_category
+                            </h5>
+                            <h1 class='title big w-600'>
+                                $post_title
+                            </h1>
+                            <h5 class='paragraph p-x-small w-400 text-color text-dim'>
+                                $new_trim_post
+                            </h5>
+                            <div class='user-details'>
+                                <a href='' class='para p-x-small w-400 text-color text-default text-capitalize'>$post_author</a>
+                                <h5 class='para p-x-small w-400 text-color text-dim'>$date_month $date_day, $date_year</h5>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </a>
+            </li>
+            ";
+        }
+    } else {
+        echo "
+        <li class='col-md-12 col-sm-12'>
+            <a href=''>
+                <div class='blog-card'>
+                    <div class='blog-body'>
+                        <h1 class='title big w-600 text-center text-upper'>
+                            no posts to show
+                        </h1>
+                    
+                    </div>
+                </div>
+            </a>
+        </li>
+        ";
+    }
+        }
+    }
+}
+
+function getUserPosts() 
 {
     global $conn;
     $userid = $_SESSION['userid'];

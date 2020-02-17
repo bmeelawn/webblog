5cd9bd4706d153cbf8262777e90b638e8ed927de
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-17 22:37:26 +0545

    include bootstrap js

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,13 +1,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.20.0/trumbowyg.min.js" integrity="sha256-oFd4Jr73mXNrGLxprpchHuhdcfcO+jCXc2kCzMTyh6A=" crossorigin="anonymous"></script>
-<script>
-    $('#editor').trumbowyg();
-    let editDrop = document.querySelector('.edit-drop');
-    let editDropMenu = document.querySelector('#edit-drop-menu');
-    editDrop.onclick = function() {
-        editDropMenu.classList.toggle('show');
-    }
-</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 

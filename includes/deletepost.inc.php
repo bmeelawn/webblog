cd6e19c6b9f9f6586cfe187985ecd024b2bc54d3
bmeelawn <60973539+bmeelawn@users.noreply.github.com>
2020-02-18 09:12:38 +0545

    add few lines logic

--- includes/deletepost.inc.php
+++ includes/deletepost.inc.php
@@ -8,9 +8,14 @@ if (!isset($_SESSION['userid'])) {
 include 'db.inc.php';
 $post_id = $_GET['id'];
 $userid = $_SESSION['userid'];
+
 $sql = "SELECT * FROM posts WHERE user_id = $userid";
 $res = mysqli_query($conn, $sql);
-if (mysqli_num_rows($res) > 0) {
+
+while ($row=mysqli_fetch_assoc($res)) {
+    $db_post_id = $row['post_id'];
+
+    if ($post_id === $db_post_id) {
     $sql = "DELETE FROM posts WHERE post_id = $post_id";
     $res = mysqli_query($conn, $sql);
     if (!$res) {
@@ -19,7 +24,29 @@ if (mysqli_num_rows($res) > 0) {
         header('Location: ../index.php?delete=success');
         exit();
     }
-} else {
+    
+    } else {
     header("Location: ../index.php?forbidden=true");
     exit();
+    }
+
 }
+
+
+
+
+// $sql = "SELECT * FROM posts WHERE user_id = $userid";
+// $res = mysqli_query($conn, $sql);
+// if (mysqli_num_rows($res) > 0) {
+//     $sql = "DELETE FROM posts WHERE post_id = $post_id";
+//     $res = mysqli_query($conn, $sql);
+//     if (!$res) {
+//         $message = "Something went wrong";
+//     } else {
+//         header('Location: ../index.php?delete=success');
+//         exit();
+//     }
+// } else {
+//     header("Location: ../index.php?forbidden=true");
+//     exit();
+// }
